3fdf12576919667a8a6e6c413786092eec5b41af
steph-ayo <popoolastephen19@gmail.com>
2025-06-05 23:54:49 +0100

    nav-linkand main header js file update

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
+import NavLink from "./nav-link";
 
 function MainHeader() {
   return (
@@ -18,12 +19,10 @@ function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals" className="">
-                Browse Meals
-              </Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
